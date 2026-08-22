4bf11c96116372e5ac720c5ed0330b4e27192f63
cyclope205 <71901181+cyclope205@users.noreply.github.com>
2026-08-22 13:39:52 +0200

    Create const.py

---
+++ custom_components/changelog_traduction/const.py
@@ -0,0 +1,6 @@
+"""Constants for the Changelog Traduction integration."""
+from __future__ import annotations
+
+DOMAIN = "changelog_traduction"
+STORAGE_VERSION = 1
+STORAGE_KEY = f"{DOMAIN}_notified"
